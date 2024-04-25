8ff3a3e2dfe6e400be4e0552c45eae23c53fc4de
catpokorny <catpokorny@gmail.com>
2024-04-25 18:30:45 +0000

    change to sessionstorage instead of internal storage for log in auth

--- pages/Login Page/jsobjects/authentication/authentication.js
+++ pages/Login Page/jsobjects/authentication/authentication.js
@@ -1,12 +1,17 @@
 export default {
-	signIn: () => {
-		return sign_in.run()
-			.then(data => {
-				delete data.user;
-				Object.keys(data).forEach(i => {
-					storeValue(i, data[i]);
-				});
-			})
-		  .then(() => navigateTo('AdminPage'))
-	},
+    signIn: () => {
+        return sign_in.run()
+            .then(data => {
+                delete data.user;
+                Object.keys(data).forEach(i => {
+                    sessionStorage.setItem(i, data[i]); // Store in sessionStorage
+                });
+                navigateTo('AdminPage');
+            })
+            .catch(error => {
+                console.error('Sign-in failed:', error);
+                // Handle error and provide feedback to the user
+                console.log('Sign-in failed. Please check your credentials.');
+            });
+    },
 }
